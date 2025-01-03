1ea27dc6da294cc57dba47a95329661a6430285b
Puthi-sgr <chethaputhi@gmail.com>
2025-01-03 13:17:29 +0700

    Fixed the URI config

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,49 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const Database_1 = __importDefault(require("./services/Database"));
 const ExpressApp_1 = __importDefault(require("./services/ExpressApp"));
 const config_1 = require("./config");
-const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
+const startServer = () =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const app = (0, express_1.default)();
     console.clear();
     console.log("Setting up database...");
@@ -25,8 +52,8 @@ const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
     yield (0, ExpressApp_1.default)(app);
     const port = config_1.PORT || 3000;
     app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
+      console.log(`Server is running on port ${port}`);
     });
-});
+  });
 startServer();
 //# sourceMappingURL=index.js.map
